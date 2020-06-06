3155edcfab333c16ef49be15701438cfdd1caee0
gintaras-martinaitis <66372488+gintaras-martinaitis@users.noreply.github.com>
2020-06-06 18:48:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/filees/usr/bin/bash
-apt update > /dev/null 2>&1 && apt --assume-yes upgrade
+#apt update > /dev/null 2>&1 && apt --assume-yes upgrade
 
-apt --assume-yes install nano php python
+#apt --assume-yes install nano php python
 
 #echo 'PS1=\u $(pwd) \$: ' >> /data/data/com.termux/files/usr/etc/bash.bashrc
 
