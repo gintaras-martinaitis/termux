44aae2f4bed18b62f9096e5259fd9736661b2d9a
gintaras-martinaitis <66372488+gintaras-martinaitis@users.noreply.github.com>
2020-06-06 18:37:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ apt update > /dev/null 2>&1 && apt --assume-yes upgrade
 apt --assume-yes install nano php python
 
 echo 'PS1=\u $(pwd) \$: ' >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo  | sed 's/test/another test/'
 clear
 cat /data/data/com.termux/files/usr/etc/bash.bashrc
