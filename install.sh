5f4fded7604f2ebd1b094f037185d624f1694880
gintaras-martinaitis <66372488+gintaras-martinaitis@users.noreply.github.com>
2020-06-06 20:15:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 apt update > /dev/null 2>&1 && apt --assume-yes upgrade
 termux-setup-storage
 apt --assume-yes install nano php python
-sed -i -- 's!$ !\u $(pwd) \$: !g' /data/data/com.termux/files/usr/etc/bash.bashrc
+sed -i -- 's!$ !\u $(pwd) \$: 
 
-echo "alias 'dir=ls -al'
-alias 'mkdir=mkdir -p'
-alias 'rc=nano /data/data/com.termux/files/usr/etc/bash.bashrc && source /data/data/com.termux/files/usr/etc/bash.bashrc'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+alias \'dir=ls -al\'
+alias \'mkdir=mkdir -p\'
+alias \'rc=nano /data/data/com.termux/files/usr/etc/bash.bashrc && source /data/data/com.termux/files/usr/etc/bash.bashrc\'
+!g' /data/data/com.termux/files/usr/etc/bash.bashrc
